46787f5ccb31ed781278b4f09534eec1c8cf42e9
yuma <yuma.shomen@googlemail.com>
2022-02-16 01:41:07 +0900

    created playerinfo.py, contains a bunch of player variable such as perks, levels, ability damage, prayers,...

---
+++ playerInfo.py
@@ -0,0 +1,55 @@
+#player stat, armour
+DAMAGE = 3270
+ACCURACY = 4479
+DUALWIELD = 0 #0 if 2h weapon, 1 if dual wield
+
+#player level
+STRENGTH = 99
+RANGED = 99
+MAGIC = 99
+
+#boosts
+OVERLOADTYPE = 3 #0 if not using overloads, 1 for normal ovl, 2 for supreme ovl, 3 for elder ovl
+TURMOILPRAYER = 2 #0 if not using damage boosting prayer, 1 if turmoil variant, 2 if praesul variant
+WEAPONPOISON = 3 #0 if not using weapon poison, 1 if +, 2 if ++, 3 if +++
+CINDERBANE = 1 #0 if not using, 1 if using cinderbane gloves
+REAVERSRING = 1
+KALDEMON = 0 #0 if not using, 1 if using kalgelion demon
+KALDEMONSCROLL = 0 #0 if not using, 1 if using kalg demon scroll
+VIGOUR = 0 #0 if not using, 1 if using ring of vigour
+OTHERDMGMULTIPLIER = 1*1 #multiply all other multiplier. e.g. maniacle and slayer helmet = 1.1 * 1.45.
+#list of multipliers found here : https://runescape.wiki/w/Ability_damage#Other_boosts
+
+#invention perks
+RELENTLESS = 5
+RELENTLESSGEARLV = 20
+RELENTLESSCD = 30 #seconds
+
+IMPATIENT = 4
+IMPATIENTGEARLV = 20
+
+BITING = 4
+BITINGGEARLV = 20
+
+INVIGORATING = 0
+INVIGORATINGGEARLV = 20
+
+PRECISE = 0
+EQUI = 4
+AFTERSHOCK = 4
+PLANTEDFEET = 1
+RUTHELESS = 3
+RUTHELESSSTACK = 5
+LUNGING = 0
+#aura
+INSPIRATIONAURA = 0 #1 if using inspiration aura, 0 if not
+
+#arch relics
+FURYOFTHESMALL = 1 # 0 if inactive, 1 if active
+CONSERVATIONOFENERGY = 0 #0 if not using, 1 if using the relic "conservation of energy"
+
+#other
+INITADREN = 100.0 #starting amount of adrenaline, float
+CANNON = 10 #0 if not using, 1 if kinetic cyclone, 2 if oldak coil, 3 if dwarf multi cannon, *10 if upgraded
+AVERAGENENEMIES = 3 #average number of enemies you attack at once
+HITCHANCE = 1 #hit chance agianst enemies
