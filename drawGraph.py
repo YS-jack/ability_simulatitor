821280eaa455c1328694b7d235926ee5d52ed12e
yuma <yuma.shomen@googlemail.com>
2022-02-17 04:31:44 +0900

    fixed small bug in last one, i think

--- drawGraph.py
+++ drawGraph.py
@@ -1,6 +1,15 @@
-class makeGraph:
+import plotly.express as px
+
+class makeGraph():
     def psCompare(dmgP,dmgS):
-        pass
+        df = px.data.tips()
+        fig = px.histogram(df, x="sex", y="total_bill",
+             color='smoker', barmode='group',
+             histfunc='avg',
+             height=400)
+        print(fig.to_dict())
+        fig.show()
+        
 
     def pDetail(dmgP):
         pass
