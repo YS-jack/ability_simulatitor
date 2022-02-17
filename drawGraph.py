606fc48c803c518a5ba88fa49658fdff74dfc71e
yuma <yuma.shomen@googlemail.com>
2022-02-17 21:11:36 +0900

    shows 1 graph comparing damage on primary and secondary targets

--- drawGraph.py
+++ drawGraph.py
@@ -1,15 +1,35 @@
+import plotly.graph_objects as go
 import plotly.express as px
 
 class makeGraph():
     def psCompare(dmgP,dmgS):
-        df = px.data.tips()
-        fig = px.histogram(df, x="sex", y="total_bill",
-             color='smoker', barmode='group',
-             histfunc='avg',
-             height=400)
-        print(fig.to_dict())
-        fig.show()
+        x = list(range(len(dmgP)))
+        yP = []
+        for hitsInTick in dmgP:
+            dmgInTick = 0
+            for abiIndex in hitsInTick:
+                dmgInTick += sum(hitsInTick[abiIndex])
+            yP.append(dmgInTick)
+        yS = []
+        for hitsInTick in dmgS:
+            dmgInTick = 0
+            for abiIndex in hitsInTick:
+                dmgInTick += sum(hitsInTick[abiIndex])
+            yS.append(dmgInTick)
         
+        fig = go.Figure(data=[go.Bar(
+            name = "Damage to primary target",
+            x = x,
+            y = yP
+            ),
+            go.Bar(
+            name = "Damage to secondary targets",
+            x = x,
+            y = yS
+            )
+        ])
+        fig.update_layout(bargap=0)
+        fig.show()
 
     def pDetail(dmgP):
         pass
